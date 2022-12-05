8df57be2e2080e51c0f5fad617946bed7cb2f24c
CameronHumphreysGIT <Cam117version@gmail.com>
2022-12-05 11:12:34 -0500

    Initial commit
    pom.xml and a  basic springboot application.

---
+++ src/main/java/CrazyEightsApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CrazyEightsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyEightsApplication.class, args);
+    }
+}
