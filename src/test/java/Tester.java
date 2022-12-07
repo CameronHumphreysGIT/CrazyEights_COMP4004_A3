5a046f8e30d10ba391efcca2ef6cf4ba65e6f48e
Cameron Humphreys <32308372+CameronHumphreysGIT@users.noreply.github.com>
2022-12-07 15:06:07 -0500

    A TEST commit
    acceptance level test for line 41

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes= CrazyEightsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -51,17 +54,13 @@ class Tester {
             //player1 has option to close lobby
             assertEquals("Three total players, would you like to wait for another?", driver1.findElement(By.id("status")).getText());
             driver1.findElement(By.id("No")).click();
-            try {
-                Thread.sleep(100);
-            }  catch(Exception e) {
-                System.out.println(e.getMessage());
-            }
+            //wait
+            myWait(0.1);
+
             assertEquals("In Game", driver1.findElement(By.id("status")).getText());
-            try {
-                Thread.sleep(1000);
-            }  catch(Exception e) {
-                System.out.println(e.getMessage());
-            }
+            //wait
+            myWait(1.0);
+
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver1.findElement(By.id("status")).getText());
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver2.findElement(By.id("status")).getText());
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver3.findElement(By.id("status")).getText());
@@ -89,11 +88,10 @@ class Tester {
             //player1 has option to close lobby
             assertEquals("Three total players, would you like to wait for another?", driver1.findElement(By.id("status")).getText());
             driver1.findElement(By.id("Yes")).click();
-            try {
-                Thread.sleep(100);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            //wait
+            myWait(0.1);
+
+
             assertEquals("Waiting...2 other players", driver1.findElement(By.id("status")).getText());
             assertEquals("Waiting...2 other players", driver2.findElement(By.id("status")).getText());
             assertEquals("Waiting...2 other players", driver3.findElement(By.id("status")).getText());
@@ -119,11 +117,7 @@ class Tester {
             WebDriver driver2 = playerJoin("Mike", 2);
             WebDriver driver3 = playerJoin("Ross", 3);
             driver1.findElement(By.id("No")).click();
-            try {
-                Thread.sleep(3000);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            myWait(3.0);
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver1.findElement(By.id("status")).getText());
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver2.findElement(By.id("status")).getText());
             assertEquals("In Game, Round1, Player1's turn turn order: left(incrementing), next: 2", driver3.findElement(By.id("status")).getText());
@@ -140,6 +134,41 @@ class Tester {
             gc.reset();
         }
     }
+    @Nested
+    @DisplayName("AcceptanceTests")
+    class AcceptanceTests {
+        @Test
+        @DisplayName("41Test")
+        void FortyOneTest() {
+            //have four players join
+            WebDriver[] drivers = fourPlayersJoin(new String[] {"Cam", "Matt", "Alexander", "Cierra"});
+            //set player 1's cards
+            gc.setCards(new ArrayList<>(Arrays.asList("5H", "KD", "3C", "9S", "JD")));
+            //set the top card so we can play 3C
+            gc.setTop("6C");
+            //check that it's playable
+            assertNotEquals(0, drivers[0].findElement(By.xpath("//button[text()='3C']")).getSize());
+            //then, play that card:
+            drivers[0].findElement(By.xpath("//button[text()='3C']")).click();
+            //check that we changed the top card
+            assertEquals("3C", drivers[0].findElement(By.id("topCard")).getText());
+            assertEquals("3C", drivers[1].findElement(By.id("topCard")).getText());
+            assertEquals("3C", drivers[2].findElement(By.id("topCard")).getText());
+            assertEquals("3C", drivers[3].findElement(By.id("topCard")).getText());
+            //check that the player has changed to player 2
+            assertEquals("In Game, Round1, Player2's turn turn order: left(incrementing), next: 3", drivers[0].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order: left(incrementing), next: 3", drivers[1].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order: left(incrementing), next: 3", drivers[2].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order: left(incrementing), next: 3", drivers[3].findElement(By.id("status")).getText());
+            //good stuff.
+            //teardown
+            drivers[0].close();
+            drivers[1].close();
+            drivers[2].close();
+            drivers[3].close();
+            gc.reset();
+        }
+    }
     //helpers
     WebDriver playerJoin(String name, int num) {
         //fixture
@@ -147,34 +176,41 @@ class Tester {
         WebDriver driver = wdl.getChromeDriver();
         driver.get("http://localhost:" + port);
 
-        try {
-            Thread.sleep(2000);
-        }
-        catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
+        myWait(2.0);
         //type in a name
         driver.findElement(By.id("name")).sendKeys(name);
         //wait a bit
-        try {
-            Thread.sleep(500);
-        }
-        catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
+        myWait(0.5);
         //click send
         driver.findElement(By.id("join")).click();
         //wait a lot
-        try {
-            Thread.sleep(4000);
-        }
-        catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
+        myWait(4.0);
         //make sure we are now on the game page
         assertEquals("http://localhost:" + port + "/game.html", driver.getCurrentUrl());
         //make sure it says my name and player number
         assertEquals("Welcome " + name + " to Crazy Eights, you are Player" + num, driver.findElement(By.id("welcome")).getText());
         return driver;
+    }
+    WebDriver[] fourPlayersJoin(String[] names) {
+        WebDriverLibrary wdl = new WebDriverLibrary();
+        WebDriver[] drivers = new WebDriver[4];
+        for(int i = 0; i < 4; i++) {
+            drivers[i] = wdl.getChromeDriver();
+            drivers[i].get("http://localhost:" + port);
+            myWait(1.0);
+            drivers[i].findElement(By.id("name")).sendKeys(names[i]);
+            myWait(0.5);
+            drivers[i].findElement(By.id("join")).click();
+            //it'll change pages
+        }
+        //good to go lol
+        return drivers;
+    }
+    void myWait(double seconds) {
+        try {
+            Thread.sleep((long) (1000 * seconds));
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
