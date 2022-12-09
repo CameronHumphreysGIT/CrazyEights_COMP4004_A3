503689970e5b6d3b28782caded8aec86094eb00a
Cameron Humphreys <32308372+CameronHumphreysGIT@users.noreply.github.com>
2022-12-09 12:28:20 -0500

    A TEST commit
    line 61 acceptance test, requires some new features, in the following code commit.

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -661,7 +661,7 @@ class Tester {
             gc.setDraw("7H");
             //draw the 7 of hearts
             drivers[0].findElement(By.id("draw")).click();
-            //make sure we can't play it
+            //make sure we can play it
             try {
                 assertTrue(drivers[0].findElement(By.xpath("//button[text()='7H']")).isDisplayed());
             } catch (NoSuchElementException e) {
@@ -677,6 +677,75 @@ class Tester {
             assertEquals("7H", drivers[1].findElement(By.id("topCard")).getText());
             assertEquals("7H", drivers[2].findElement(By.id("topCard")).getText());
             assertEquals("7H", drivers[3].findElement(By.id("topCard")).getText());
+            //teardown
+            drivers[0].close();
+            drivers[1].close();
+            drivers[2].close();
+            drivers[3].close();
+            gc.reset();
+        }
+        @Test
+        @DisplayName("61Test")
+        void SixtyOneTest() {
+            //have four players join
+            WebDriver[] drivers = fourPlayersJoin(new String[]{"Cam", "Matt", "Alexander", "Cierra"});
+            //set the top card so we can play
+            gc.setTopCard("7C");
+            gc.setCards(new ArrayList<>(Arrays.asList("3H")), 1);
+            //set the next card we will draw
+            gc.setDraw("6D");
+            gc.refresh();
+            //check that it's not playable
+            try {
+                assertTrue(drivers[0].findElement(By.xpath("//button[text()='3H']")).isDisplayed());
+            } catch (NoSuchElementException e) {
+                //good
+                assertTrue(true);
+            }
+            //click the draw button
+            drivers[0].findElement(By.id("draw")).click();
+            //we get 6D, show it...
+            try {
+                assertTrue(drivers[0].findElement(By.xpath("//button[text()='6D']")).isDisplayed());
+            } catch (NoSuchElementException e) {
+                //good
+                assertTrue(true);
+            }
+            //got the 6 of diamonds, set before we draw again.
+            gc.setDraw("5S");
+            //draw the 5 of spades
+            drivers[0].findElement(By.id("draw")).click();
+            //make sure we can't play it
+            try {
+                assertTrue(drivers[0].findElement(By.xpath("//button[text()='5S']")).isDisplayed());
+            } catch (NoSuchElementException e) {
+                //good
+                assertTrue(true);
+            }
+            //got the 5 of spades, set before we draw again.
+            gc.setDraw("4H");
+            //draw the 4 of hearts
+            drivers[0].findElement(By.id("draw")).click();
+            //make sure we can't play it
+            try {
+                assertTrue(drivers[0].findElement(By.xpath("//button[text()='4H']")).isDisplayed());
+            } catch (NoSuchElementException e) {
+                //good
+                assertTrue(true);
+            }
+            //make sure we no longer can draw cards...
+            assertFalse(drivers[0].findElement(By.id("draw")).isEnabled());
+            //turn ends since we drew three, topCard is the same
+            //check that we didn't change the top card
+            assertEquals("7C", drivers[0].findElement(By.id("topCard")).getText());
+            assertEquals("7C", drivers[1].findElement(By.id("topCard")).getText());
+            assertEquals("7C", drivers[2].findElement(By.id("topCard")).getText());
+            assertEquals("7C", drivers[3].findElement(By.id("topCard")).getText());
+            //next turn
+            assertEquals("In Game, Round1, Player2's turn turn order:left(incrementing), next: 3", drivers[0].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order:left(incrementing), next: 3", drivers[1].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order:left(incrementing), next: 3", drivers[2].findElement(By.id("status")).getText());
+            assertEquals("In Game, Round1, Player2's turn turn order:left(incrementing), next: 3", drivers[3].findElement(By.id("status")).getText());
             //teardown
             drivers[0].close();
             drivers[1].close();
